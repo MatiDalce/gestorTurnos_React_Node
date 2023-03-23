8310f90031bb8a7a82861543f4c4c500db4c118e
Matias <matigodoy93@hotmail.com>
2023-03-23 16:33:49 -0300

    Componente Inicializado:  Navbar

--- frontend/src/components/Input/Input.jsx
+++ frontend/src/components/Input/Input.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './input.css';
 
 export const Input = () => {
     return (
--- frontend/src/components/Table/Table.jsx
+++ frontend/src/components/Table/Table.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './table.css';
 
 export const Table = () => {
   return (
