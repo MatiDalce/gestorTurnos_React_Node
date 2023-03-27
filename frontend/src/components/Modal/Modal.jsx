846273c7cf7eda428f4e902aba6572ed4314b4be
Matias <matigodoy93@hotmail.com>
2023-03-27 20:58:02 -0300

    Avances en calendario

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,98 +1,106 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Table from '../Table/Table'
 import Title from '../Title/Title'
 import CrossIcon from '../../assets/icons/xmark-solid.svg'
 import './modal.css'
 
-const Modal = () => {
+// INFO DEL CALENDARIO
+// EVENT CLICK - https://fullcalendar.io/docs/eventClick
+// EVENT DATA - https://fullcalendar.io/docs/v3/event-data
+
+const Modal = ({title, description}) => {
+    const [openModal, setOpenModal] = useState(false);
 
     const closeModal = () => {
-        console.log('Cerrar modal');
+        setOpenModal(!openModal)
     }
 
     return (
-        <div className="modal-container">
-            <div className='divCloseModal' onClick={closeModal}>
-                <img className='closeModal' src={CrossIcon} alt='Cerrar' />
+        <div className="modal-center">
+            <div className="modal-container">
+                <div className='divCloseModal' onClick={closeModal}>
+                    <img className='closeModal' src={CrossIcon} alt='Cerrar' />
+                </div>
+                <Title 
+                    title={title}
+                    isBold
+                    textColor='var(--white-bg)'
+                />
+                <p className='appointment'>{description || ''}</p>
+                {/* <Table 
+                    headerBg='var(--skyblue-bg)'
+                    headers={['ID','Nombre y Apellido', 'Hora']}
+                    contentDisplay={['id', 'name', 'time']}
+                    noClickable
+                    noSticky
+                    noCursor
+                    tableWidth={'100%'}
+                    textColor='var(--white-bg)'
+                    noTableBorders
+                    content={[
+                        {
+                            id: 1,
+                            name: 'Diego Perez',
+                            time: '13:30hs'
+                        },
+                        {
+                            id: 2,
+                            name: 'Diego Perez',
+                            time: '13:30hs'
+                        },
+                        {
+                            id: 3,
+                            name: 'Diego Perez',
+                            time: '13:30hs'
+                        },
+                        {
+                            id: 4,
+                            name: 'Diego Perez',
+                            time: '13:30hs'
+                        },
+                        {
+                            id: 5,
+                            name: 'Diego Perez',
+                            time: '13:30hs'
+                        },
+                        {
+                            id: 6,
+                            name: 'Diego Perez',
+                            time: '13:30hs'
+                        },
+                        {
+                            id: 1,
+                            name: 'Diego Perez',
+                            time: '13:30hs'
+                        },
+                        {
+                            id: 2,
+                            name: 'Diego Perez',
+                            time: '13:30hs'
+                        },
+                        {
+                            id: 3,
+                            name: 'Diego Perez',
+                            time: '13:30hs'
+                        },
+                        {
+                            id: 4,
+                            name: 'Diego Perez',
+                            time: '13:30hs'
+                        },
+                        {
+                            id: 5,
+                            name: 'Diego Perez',
+                            time: '13:30hs'
+                        },
+                        {
+                            id: 6,
+                            name: 'Diego Perez',
+                            time: '13:30hs'
+                        },
+                    ]}
+                /> */}
             </div>
-            <Title 
-                title='Fecha: 03/06' 
-                isBold
-                textColor='var(--white-bg)'
-            />
-            <Table 
-                headerBg='var(--skyblue-bg)'
-                headers={['ID','Nombre y Apellido', 'Hora']}
-                contentDisplay={['id', 'name', 'time']}
-                noClickable
-                noSticky
-                noCursor
-                tableWidth={'100%'}
-                textColor='var(--white-bg)'
-                noTableBorders
-                content={[
-                    {
-                        id: 1,
-                        name: 'Diego Perez',
-                        time: '13:30hs'
-                    },
-                    {
-                        id: 2,
-                        name: 'Diego Perez',
-                        time: '13:30hs'
-                    },
-                    {
-                        id: 3,
-                        name: 'Diego Perez',
-                        time: '13:30hs'
-                    },
-                    {
-                        id: 4,
-                        name: 'Diego Perez',
-                        time: '13:30hs'
-                    },
-                    {
-                        id: 5,
-                        name: 'Diego Perez',
-                        time: '13:30hs'
-                    },
-                    {
-                        id: 6,
-                        name: 'Diego Perez',
-                        time: '13:30hs'
-                    },
-                    {
-                        id: 1,
-                        name: 'Diego Perez',
-                        time: '13:30hs'
-                    },
-                    {
-                        id: 2,
-                        name: 'Diego Perez',
-                        time: '13:30hs'
-                    },
-                    {
-                        id: 3,
-                        name: 'Diego Perez',
-                        time: '13:30hs'
-                    },
-                    {
-                        id: 4,
-                        name: 'Diego Perez',
-                        time: '13:30hs'
-                    },
-                    {
-                        id: 5,
-                        name: 'Diego Perez',
-                        time: '13:30hs'
-                    },
-                    {
-                        id: 6,
-                        name: 'Diego Perez',
-                        time: '13:30hs'
-                    },
-                ]}
-            />
         </div>
     )
 }
--- frontend/src/pages/MyCalendar/MyCalendar.jsx
+++ frontend/src/pages/MyCalendar/MyCalendar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -9,6 +9,9 @@ import './myCalendar.css';
 const MyCalendar = ({ eventsList }) => {
 
   // FALTA QUE AL HACER CLICK EN UNA FECHA TE DEVUELVA LA DATA DE ESE EVENTO PARA MANDARLA AL MODAL
+
+  const [openModal, setOpenModal] = useState(false)
+  const [modalTitle, setModalTitle] = useState(false)
 
   const hardcodeEvents = [
     { 
@@ -22,17 +25,39 @@ const MyCalendar = ({ eventsList }) => {
       title: 'Evento 2',
       start: '2023-03-27T14:00:00',
       end: '2023-03-27T16:00:00',
-    }
+    },
+    { 
+      id: 3,
+      title: 'Evento 2',
+      start: '2023-03-28T14:00:00',
+      end: '2023-03-28T16:00:00',
+    },
+    { 
+      id: 4,
+      title: 'Evento 4',
+      start: '2023-03-29T17:00:00',
+      end: '2023-03-29T18:00:00',
+    },
   ];
 
   const handleDateClick = (e) => {
-    // console.log(e.date); // Fecha formato largo
-    // console.log(e.dateStr); // Fecha formato string yyyy-mm-dd
-    console.log(e.view);
+    console.log(e.date); // Fecha formato largo
+    console.log(e.dateStr); // Fecha formato string yyyy-mm-dd
+    
+  }
+
+  function handleEventOnClick(eventInfo) {
+    // console.log('eventInfo => ', eventInfo); // El evento completo
+    // console.log('eventInfo => ', eventInfo.el.fcSeg.eventRange.def.title); // Titulo
+    setModalTitle(eventInfo.el.fcSeg.eventRange.def.title)
+    setOpenModal(true)
   }
 
   return (
         <div className="calendar-box">
+          {
+            openModal && <Modal title={modalTitle} />
+          }
           <FullCalendar 
             events={hardcodeEvents}
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
@@ -43,9 +68,9 @@ const MyCalendar = ({ eventsList }) => {
               end: "dayGridMonth, timeGridWeek, timeGridDay",
             }}
             dateClick={handleDateClick} // Da info sobre el día
+            eventClick={handleEventOnClick} // Da info sobre el evento
             // loading={isLoading}
             // weekends={false} // Si no quiere fines de semana
-            
           />
         </div>
   )
