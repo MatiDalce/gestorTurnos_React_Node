7be7185c35a6cbc55ca4ad9b8985a8a1e9cf7649
Matias <matigodoy93@hotmail.com>
2023-03-31 16:33:30 -0300

    Funcionalidad Toasts

---
+++ frontend/src/assets/helpers/toast.js
@@ -0,0 +1,22 @@
+import Swal from 'sweetalert2';
+
+// type: STRING | "success" o "error"
+// title: STRING | Es el texto
+export function toast (type, title) {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+    
+    return Toast.fire({
+        icon: type,
+        title: title
+    })
+} 
