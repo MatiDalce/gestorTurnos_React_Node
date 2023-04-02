3f97f159879ec7345f4ab1717f5e22e7ce8ad99b
Matias <matigodoy93@hotmail.com>
2023-04-02 05:56:34 -0300

    GET Filtro de turnos de paciente

--- frontend/src/pages/ShiftPatientList/ShiftPatientList.jsx
+++ frontend/src/pages/ShiftPatientList/ShiftPatientList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Input from '../../components/Input/Input'
 import Select from '../../components/Select/Select'
 import Table from '../../components/Table/Table'
@@ -6,26 +7,43 @@ import { config } from '../../env/config';
 import './shiftPatientList.css'
 
 const ShiftPatientList = () => {
+
+  const { id } = useParams()
+
   const [shiftPatientList, setShiftPatientList] = useState([]);
 
   const handleDateFrom = () => {}
-  const handleDateUntil = () => {}
-  const handleOrder = () => {}
 
+  const handleDateUntil = () => {}
+
+  const handleOrder = (e) => {
+    if(e.target.value === 'recientes') {
+      fetch(`${config.webAPI}/patients/patient-appointmentsDSC/${id}`)
+      .then(res => res.json())
+      .then(res => {
+        setShiftPatientList(res)
+      });
+    } else if(e.target.value === 'antiguos') {
+      fetch(`${config.webAPI}/patients/patient-appointments/${id}`)
+      .then(res => res.json())
+      .then(res => {
+        setShiftPatientList(res)
+      });
+    } else return;
+  }
 
   useEffect(() => {
-    fetch(`${config.webAPI}/`)
+    fetch(`${config.webAPI}/patients/patient-appointments/${id}`)
     .then(res => res.json())
     .then(res => {
-      console.log(res);
       setShiftPatientList(res)
     });
-  }, [])
+  }, [id])
 
   return (
     <>
     <div className="search-patient-shifts">
-      <div className="shiftPatientList-input-box">
+      {/* <div className="shiftPatientList-input-box">
         <Input 
           onChange={handleDateFrom}
           hasLabel
@@ -42,11 +60,15 @@ const ShiftPatientList = () => {
           type='date'
           nameProp='untilDate'
         />
-      </div>
+      </div> */}
       <div className="shiftPatientList-input-box">
         <Select
           onChange={handleOrder}
           options={[
+            {
+              text: 'Seleccione',
+              value: ''
+            },
             {
               text: 'De más antiguos a más recientes',
               value: 'antiguos'
@@ -64,130 +86,9 @@ const ShiftPatientList = () => {
     </div>
     <Table 
       staticPath={'/turno'}
-      headers={['N° de turno', 'Fecha', 'Estado']} 
-      contentDisplay={['id', 'appointment', 'date', 'status']}
-      content={[
-        {
-          id: 1,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Pendiente',
-        },
-        {
-          id: 2,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Pendiente'
-        },
-        {
-          id: 3,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Pendiente'
-        },
-        {
-          id: 4,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Pendiente'
-        },
-        {
-          id: 5,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Pendiente'
-        },
-        {
-          id: 6,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Pendiente'
-        },
-        {
-          id: 7,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Pendiente',
-        },
-        {
-          id: 8,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Pendiente'
-        },
-        {
-          id: 9,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Pendiente'
-        },
-        {
-          id: 10,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Realizado'
-        },
-        {
-          id: 11,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Realizado'
-        },
-        {
-          id: 12,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Realizado'
-        },
-        {
-          id: 1,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Realizado'
-        },
-        {
-          id: 13,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Realizado'
-        },
-        {
-          id: 14,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Realizado',
-        },
-        {
-          id: 15,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Realizado'
-        },
-        {
-          id: 16,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Realizado'
-        },
-        {
-          id: 17,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Realizado'
-        },
-        {
-          id: 18,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Realizado'
-        },
-        {
-          id: 19,
-          shiftNumber: '128523532',
-          date: '13/06/2023',
-          state: 'Realizado'
-        },
-      ]} 
+      headers={['Fecha', 'Hora']} 
+      contentDisplay={['day', 'hour']}
+      content={shiftPatientList || []} 
     />
   </>
   )
