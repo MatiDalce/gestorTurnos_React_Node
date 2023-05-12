8f29c487f84bde15db04824459ab84cca0e6495c
MaxiRebolo <maximilianorebolo@gmail.com>
2023-05-12 19:13:47 -0300

    agregando firebase

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-usYuGJ0FLLmBP_XGX-oiDQJOc2JflCg",
+  authDomain: "gestorpsico.firebaseapp.com",
+  projectId: "gestorpsico",
+  storageBucket: "gestorpsico.appspot.com",
+  messagingSenderId: "819222606109",
+  appId: "1:819222606109:web:465bb385514d587b1ab1f4",
+  measurementId: "G-XHTP76NTJP"
+};
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
