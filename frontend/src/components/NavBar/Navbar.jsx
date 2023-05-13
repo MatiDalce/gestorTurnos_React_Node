7ca84736267f8bb8849ccfffaddbe95b08e56867
Matias <matigodoy93@hotmail.com>
2023-05-13 03:08:35 -0300

    Menu hamburguesa

--- frontend/src/components/NavBar/Navbar.jsx
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 import { Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
-import Swal from 'sweetalert2';
-import { toast } from '../../assets/helpers/toast'
-import { config } from '../../env/config';
 import { CurrentPage } from '../../assets/helpers/CurrentPage';
 import ArrowLogo from '../../assets/icons/arrow-left-solid.svg';
 import eyeLogo from '../../assets/icons/eye-solid.svg';
+import { SideBar } from '../SideBar/SideBar';
 import './navbar.css';
 
 const Navbar = () => {
@@ -18,56 +16,6 @@ const Navbar = () => {
     // Ocultar toda la pantalla
     const handleWatch = () => {
         setWatch(!watch)
-    }
-
-    // Ocultar toda la pantalla
-    const handleDownloadAppointments = () => {
-        fetch(`${config.webAPI}/appointments/download`)
-        .then(response => {
-            if (!response.ok) {
-                toast('error', 'Ha ocurrido un error en la descarga')
-                throw new Error('Falló la descarga');
-            }
-            return response.blob();
-        })
-        .then(blob => {
-            if(blob) {
-                const file = new File([blob], 'archivo.zip', { type: 'application/zip' })
-                // Create a URL for the Blob object
-                const url = URL.createObjectURL(file);
-                // Create a link element and click it to download the file
-                const link = document.createElement('a');
-                link.href = url;
-                link.download = `Todos_los_turnos.zip`;
-                document.body.appendChild(link);
-                link.click();
-                document.body.removeChild(link);
-                toast('success', 'Documento descargado exitosamente');
-            } else {
-                toast('error', 'Ha ocurrido un error en la descarga')
-            }
-        })
-    }
-
-    // Cerrar sesión
-    const handleSession = () => {
-        return Swal.fire({
-            title: 'Está a punto de cerrar sesión',
-            text: '¿Está seguro?',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: 'var(--skyblue-bg)',
-            cancelButtonColor: 'var(--red-bg)',
-            confirmButtonText: 'Aceptar',
-            cancelButtonText: 'Cancelar'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                localStorage.removeItem('auth')
-                navigate('/login') 
-            } else {
-                return;
-            }
-        })
     }
 
     return (
@@ -90,23 +38,8 @@ const Navbar = () => {
                         <h1 className="navbar-title">{<CurrentPage pathname={location.pathname} id={id} />}</h1>
                     </div>
 
-                    <div className="hamburguer-container">
-                        <div className="hamburger-menu">
-                            <input id="menu__toggle" type="checkbox" />
-                            <label className="menu__btn" htmlFor="menu__toggle">
-                            <span></span>
-                            </label>
-
-                            <ul className="menu__box">
-                                <li className="menu__item" onClick={() => navigate('/')}>Inicio</li>
-                                <li className="menu__item" onClick={handleWatch}>Modo oculto</li>
-                                <li className="menu__item" onClick={() => navigate('/listado-pacientes')}>Listado de pacientes</li>
-                                <li className="menu__item" onClick={() => navigate('/mi-calendario')}>Calendario</li>
-                                <li className="menu__item" onClick={() => navigate('/listado-turnos')}>Listado de turnos</li>
-                                <li className="menu__item" onClick={handleDownloadAppointments}>Descargar los turnos</li>
-                                <li className="menu__item" onClick={handleSession}>Cerrar sesión</li>
-                            </ul>
-                        </div>
+                    <div className="hamburguer-container" id='hamburguer-container'>
+                        <SideBar setWatch={setWatch} watch={watch} pageWrapId={"page-wrap"} outerContainerId={"hamburguer-container"} />
                     </div>
 
                 </nav>
---
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react'
+import { slide as Menu } from "react-burger-menu";
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { toast } from '../../assets/helpers/toast';
+import { config } from '../../env/config';
+import Cross from '../../assets/icons/xmark-solid.svg'
+import lala from '../../assets/icons/arrow-left-solid.svg'
+import "./sideBar.css";
+export const SideBar = (props) => {
+    let navigate = useNavigate();
+    
+    const [watch] = useState(false);
+
+    // Ocultar toda la pantalla
+    const handleWatch = () => {
+        props.setWatch(!watch)
+    }
+
+    // Ocultar toda la pantalla
+    const handleDownloadAppointments = () => {
+        fetch(`${config.webAPI}/appointments/download`)
+        .then(response => {
+            if (!response.ok) {
+                toast('error', 'Ha ocurrido un error en la descarga')
+                throw new Error('Falló la descarga');
+            }
+            return response.blob();
+        })
+        .then(blob => {
+            if(blob) {
+                const file = new File([blob], 'archivo.zip', { type: 'application/zip' })
+                // Create a URL for the Blob object
+                const url = URL.createObjectURL(file);
+                // Create a link element and click it to download the file
+                const link = document.createElement('a');
+                link.href = url;
+                link.download = `Todos_los_turnos.zip`;
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+                toast('success', 'Documento descargado exitosamente');
+            } else {
+                toast('error', 'Ha ocurrido un error en la descarga')
+            }
+        })
+    }
+
+    // Cerrar sesión
+    const handleSession = () => {
+        return Swal.fire({
+            title: 'Está a punto de cerrar sesión',
+            text: '¿Está seguro?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: 'var(--skyblue-bg)',
+            cancelButtonColor: 'var(--red-bg)',
+            confirmButtonText: 'Aceptar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                localStorage.removeItem('auth')
+                navigate('/login') 
+            } else {
+                return;
+            }
+        })
+    }
+    return (
+        <>
+            <div id="page-wrap"></div>
+            {/* Pass on our props */}
+            <Menu {...props} >
+                    <p className='menu__item_sidebar' onClick={() => navigate('/')}>Inicio</p>
+                    <p className='menu__item_sidebar' onClick={handleWatch}>Modo oculto</p>
+                    <p className='menu__item_sidebar' onClick={() => navigate('/listado-pacientes')}>Listado de pacientes</p>
+                    <p className='menu__item_sidebar' onClick={() => navigate('/mi-calendario')}>Calendario</p>
+                    <p className='menu__item_sidebar' onClick={() => navigate('/listado-turnos')}>Listado de turnos</p>
+                    <p className='menu__item_sidebar' onClick={handleDownloadAppointments}>Descargar los turnos</p>
+                    <p className='menu__item_sidebar' onClick={handleSession}>Cerrar sesión</p>
+            </Menu>
+        </>
+    );
+}
