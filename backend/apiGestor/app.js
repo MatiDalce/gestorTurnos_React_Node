d9f67f0d8856bfb2a88986b3724897857b648d06
MATIAS DALCEGGIO <matiasdalceggio@gmail.com>
2023-05-20 00:26:23 -0300

    login2

--- backend/apiGestor/app.js
+++ backend/apiGestor/app.js
@@ -11,6 +11,7 @@ const cors = require('cors')
 var indexRouter = require('./routes/index');
 var appointmentRouter = require('./routes/appointment');
 var patientRouter = require('./routes/patient')
+var loginRouter = require("./routes/login")
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/patients', patientRouter);
 app.use('/appointments', appointmentRouter);
+app.use("/login", loginRouter )
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
